ee2bea565042e4e23fe05b8fd9aa2054ef8c33b8
goldentechie <bendam1207@gmail.com>
2020-05-31 01:00:19 +0300

    Dapp: Remove .env file from repo, add example

--- contracts/buidler.config.ts
+++ contracts/buidler.config.ts
@@ -4,16 +4,17 @@ usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("buidler-typechain");
 
 const config: BuidlerConfig = {
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:18545"
+    }
+  },
   paths: {
     artifacts: "build/contracts",
     tests: "tests"
   },
   solc: {
-    version: "0.6.4",
-    optimizer: {
-      enabled: true,
-      runs: 10000
-    }
+    version: "0.6.4"
   },
   typechain: {
     outDir: "build/types",
