b4679a280753aa9c91a4535786576008b9603914
goldentechie <bendam1207@gmail.com>
2020-10-21 16:23:37 -0400

    add about details

--- contracts/utils/deployment.ts
+++ contracts/utils/deployment.ts
@@ -1,10 +1,8 @@
-import bre from '@nomiclabs/buidler'
+import { ethers } from '@nomiclabs/buidler'
 import { Signer, Contract } from 'ethers'
 import { link } from 'ethereum-waffle'
 
 import MACIFactoryArtifact from '../build/contracts/MACIFactory.json'
-
-const ethers = (bre as any).ethers
 
 async function deployContract(
   account: Signer,
@@ -21,31 +19,21 @@ async function deployContract(
   return contract
 }
 
-export function linkBytecode(
-  bytecode: string,
-  libraries: {[name: string]: string},
-): string {
-  // Workarounds for https://github.com/nomiclabs/buidler/issues/611
-  const linkable = { evm: { bytecode: { object: bytecode } } }
-  for (const [libraryName, libraryAddress] of Object.entries(libraries)) {
-    link(linkable, libraryName, libraryAddress.toLowerCase())
-  }
-  return linkable.evm.bytecode.object
-}
-
 export async function deployMaciFactory(account: Signer): Promise<Contract> {
   const batchTreeVerifier = await deployContract(account, 'BatchUpdateStateTreeVerifier')
   const voteTallyVerifier = await deployContract(account, 'QuadVoteTallyVerifier')
   const poseidonT3 = await deployContract(account, 'PoseidonT3')
   const poseidonT6 = await deployContract(account, 'PoseidonT6')
 
-  const linkedBytecode = linkBytecode(MACIFactoryArtifact.bytecode, {
-    'maci-contracts/sol/Poseidon.sol:PoseidonT3': poseidonT3.address,
-    'maci-contracts/sol/Poseidon.sol:PoseidonT6': poseidonT6.address,
-  })
+  // Workarounds for https://github.com/nomiclabs/buidler/issues/611
+  const MACIFactoryArtifactCopy = {...MACIFactoryArtifact}
+  const linkable = { evm: { bytecode: { object: MACIFactoryArtifactCopy.bytecode } } }
+  link(linkable, 'maci-contracts/sol/Poseidon.sol:PoseidonT3', poseidonT3.address)
+  link(linkable, 'maci-contracts/sol/Poseidon.sol:PoseidonT6', poseidonT6.address)
+  MACIFactoryArtifactCopy.bytecode = linkable.evm.bytecode.object
   const MACIFactory = await ethers.getContractFactory(
-    MACIFactoryArtifact.abi,
-    linkedBytecode,
+    MACIFactoryArtifactCopy.abi,
+    MACIFactoryArtifactCopy.bytecode,
     account,
   )
 
